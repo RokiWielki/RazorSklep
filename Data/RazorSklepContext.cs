2e083f18538e6f1bdd24789c322f8eda5c388818
RokiWielki <Cruck222@interia.pl>
2023-05-12 05:29:03 +0200

    Dodaj pliki projektów.

---
+++ Data/RazorSklepContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorSklep.Models;
+
+namespace RazorSklep.Data
+{
+    public class RazorSklepContext : DbContext
+    {
+        public RazorSklepContext (DbContextOptions<RazorSklepContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorSklep.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorSklep.Data;
+using RazorSklep.Models;
+
+namespace RazorSklep.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorSklep.Data.RazorSklepContext _context;
+
+        public IndexModel(RazorSklep.Data.RazorSklepContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
